11573c0463db581a1c09996a0143452c4629b0a9
Adrien Maillol <maillol.adrien@gmail.com>
2016-04-10 00:53:11 +0200

    toggle put node mode

--- src/game.js
+++ src/game.js
@@ -8,19 +8,27 @@ Game.prototype.create = function () {
   nodes = this.game.add.group()
 
   FREE = 1
-  LINKING = 2
+  PUT_NODE = 2
+  LINKING = 3
 
   cursorStatus = FREE
-  prevNode = null
   cursorLine = null
 
   lines = []
 
   var e = new Engine()
-  e.update()
+  console.log(e)
 
   cursorNode = this.game.add.sprite(100, 100, 'node')
   cursorNode.anchor.set(0.5)
+  cursorNode.tint = 0xffaaff
+  cursorNode.visible = false
+
+  keys = this.game.input.keyboard.addKeys({
+    'node': Phaser.KeyCode.N
+  })
+
+  keys.node.onDown.add(this.togglePutNode, this)
 
 };
 
@@ -31,19 +39,32 @@ Game.prototype.snap = function (v) {
   return v - v % 20
 }
 
+Game.prototype.togglePutNode = function() {
+  if(keys.node.isDown) {
+    if(cursorStatus == FREE) {
+      cursorStatus = PUT_NODE
+        cursorNode.visible = true
+    } else if(cursorStatus == PUT_NODE) {
+      cursorStatus = FREE
+        cursorNode.visible = false
+    }
+  }
+}
+
 Game.prototype.render = function () {
-  if(cursorStatus == LINKING) {
-    //console.log(this.input.x)
+
+  if(cursorStatus == PUT_NODE) {
+    cursorNode.position.set(
+      this.snap(this.input.x),
+      this.snap(this.input.y)
+    )
+  } else if (cursorStatus == LINKING) {
     cursorLine.end.set(
       this.snap(this.input.x),
       this.snap(this.input.y)
     )
-  }
 
-  cursorNode.position.set(
-    this.snap(this.input.x),
-    this.snap(this.input.y)
-  )
+  }
 
   for(var l in lines) {
     this.game.debug.geom(lines[l])
@@ -62,23 +83,20 @@ Game.prototype.addNode = function (x, y) {
 Game.prototype.renderCell = function (x, y, size) { size = size || 20 }
 
 Game.prototype.onInputDown = function (e) {
-  //console.log(e)
-  //
-  var x = this.snap(e.x)
-  var y = this.snap(e.y)
-  var newNode = this.addNode(x, y)
 
   //console.log(cursorStatus)
   if(cursorStatus == FREE) {
-    prevNode = newNode
-    cursorStatus = LINKING
-
-    cursorLine = new Phaser.Line(x, y, x, y)
+    //cursorLine = new Phaser.Line(x, y, x, y)
 
   } else if ( cursorStatus == LINKING) {
     console.log( cursorLine.start.x, cursorLine.start.y, cursorLine.end.x, cursorLine.end.y)
     lines.push(new Phaser.Line(cursorLine.start.x, cursorLine.start.y, cursorLine.end.x, cursorLine.end.y))
     cursorStatus = FREE
+  } else if(cursorStatus == PUT_NODE) {
+    var x = this.snap(e.x)
+    var y = this.snap(e.y)
+    var newNode = this.addNode(x, y)
+
   }
 
 
