ef6bcab0199ec306d2fe017005b56f807aa2b9f3
RomanWlm <roman.wilhelm@scaledrisk.com>
2016-04-09 23:50:42 +0200

    init minimal map and log it at update

--- src/engine.js
+++ src/engine.js
@@ -1,11 +1,37 @@
-function Engine() {}
+function Engine() {
+  console.log("init engine")
+
+  this.map = new Array()
+  this.x_len = 10
+  this.y_len = 20
+
+  for (var x = 0 ; x< this.x_len ; x++){
+        this.map[x] = new Array()
+      for (var y = 0 ; y< this.y_len ; y++){
+        this.map[x][y] = this.cell()
+      }
+  }
+}
+
 Engine.prototype.update = function () {
   console.log("update")
+  for (var x = 0 ; x< this.x_len ; x++){
+      for (var y = 0 ; y< this.y_len ; y++){
+        console.log(this.map[x][y])
+      }
+  }
 }
 
 Engine.prototype.cell = function () {
+
   return {
-    key: "value",
+    //sate
+    "l_up" : false,
+    "l_down" : false,
+    "l_left" : false,
+    "l_right" : false,
+    "hasRoad" : false,
+
     "otherKey": ["this", "is", "an", "arraY"],
     anObj : { aKey : 12, b: "dd"}
   }
